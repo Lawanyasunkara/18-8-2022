ce3c159b9bfa799b58d5a4b54b3a7cc7cd561ec4
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-08-18 12:01:31 +0530

    initial

---
+++ lavConsoleApp2/lavConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lavConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            string name;
+            Console.WriteLine("Enter a string");
+            name=Console.ReadLine();
+            Console.WriteLine("Hello"+name);
+            Console.ReadLine();
+        }
+    }
+}
